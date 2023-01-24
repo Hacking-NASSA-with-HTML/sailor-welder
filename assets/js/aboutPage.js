0a5f2853e67b35e41e04db99335220e2c3be6d9b
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-24 23:40:59 +0400

    feat: add second image to About section

--- assets/js/aboutPage.js
+++ assets/js/aboutPage.js
@@ -11,7 +11,10 @@ function aboutContent() {
                 <p>About Page Content</p>
                 <p>About Page Content</p>
                 <div class="about_img_wrapper">
-                    <img data-src="./assets/img/anime_welder_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder_106_132.webp 1x, ./assets/img/anime_welder_212_264.webp 2x" class="about_img lazyload" alt="anime sailor-welder">
+                    <img data-src="./assets/img/anime_welder_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder_106_132.webp 1x, ./assets/img/anime_welder_212_264.webp 2x" class="about_img lazyload" alt="Anime sailor-welder">
+                </div>
+                <div class="about_img_wrapper">
+                    <img data-src="./assets/img/anime_welder2_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder2_106_132.webp 1x, ./assets/img/anime_welder2_212_264.webp 2x" class="about_img lazyload" alt="One more anime sailor-welder">
                 </div>
                 <p>About Page Content</p>
                 <p>About Page Content</p>
