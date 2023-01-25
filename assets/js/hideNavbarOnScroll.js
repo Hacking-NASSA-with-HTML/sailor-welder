70f05952d7cad97d10549bb4f008a39a82fe735a
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-25 17:07:00 +0400

    feat: add module highlight a tags on scroll

--- assets/js/hideNavbarOnScroll.js
+++ assets/js/hideNavbarOnScroll.js
@@ -1,9 +1,10 @@
 const headerWrapper = document.getElementById('headerWrapper')
+const sections = document.querySelectorAll('#navigation a')
 
 let lastScrollTop = 0
 
 
-window.addEventListener('scroll', () => {
+function hideNavbar() {
     let scrollTop = window.pageYOffset
     if (scrollTop > lastScrollTop) {
         headerWrapper.style.top = "-80px"
@@ -11,4 +12,24 @@ window.addEventListener('scroll', () => {
         headerWrapper.style.top = "0"
     }
     lastScrollTop = scrollTop
-})
+}
+
+function onScroll(event) {
+    let sections = document.querySelectorAll('#navigation a')
+    let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+
+    for (let i = 0; i < sections.length; i++) {
+        let currLink = sections[i]
+        let val = currLink.getAttribute('href')
+        let refElement = document.querySelector(val)
+        if (refElement.offsetTop - 400 <= scrollPos && (refElement.offsetTop - 400 + refElement.offsetHeight > scrollPos)) {
+            document.querySelector('#navigation a').classList.remove('focused')
+            currLink.classList.add('focused')
+        } else {
+            currLink.classList.remove('focused')
+        }
+    }
+}
+
+window.document.addEventListener('scroll', hideNavbar)
+window.document.addEventListener('scroll', onScroll)
