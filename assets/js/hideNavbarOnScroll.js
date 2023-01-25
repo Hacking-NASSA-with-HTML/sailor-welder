4f5b4df0d2b5a7779257ac6b98abda1712e9fa4b
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-25 15:59:31 +0400

    refactor: add translate3d prop to Hide on scroll Module

--- assets/js/hideNavbarOnScroll.js
+++ assets/js/hideNavbarOnScroll.js
@@ -4,7 +4,7 @@ let lastScrollTop = 0
 
 
 window.addEventListener('scroll', () => {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    let scrollTop = window.pageYOffset
     if (scrollTop > lastScrollTop) {
         headerWrapper.style.transform = "translate3d(0, -100%, 0)"
     } else {
