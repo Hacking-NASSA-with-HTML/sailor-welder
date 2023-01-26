09b9b22428d294fcf208d37cbc4519ac96d1d41a
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-26 09:08:23 +0400

    feat: add animation stylings to the project link

--- assets/js/contactPage.js
+++ assets/js/contactPage.js
@@ -11,8 +11,8 @@ function contactContent() {
                 <p>Customize and Sell It to Your customers!💲💸💰</p>
                 <p>Earn💲money for Yourself!</p>
                 <p>Like it? Give that repo a Star⭐</p>
-                <p>
-                    <a class="contact_section_app_link" href="https://github.com/Hacking-NASSA-with-HTML/sailor-welder/" target="_blank" rel="noopener noreferrer">Give it a Star on github</a>
+                <p class="contact_section_app_link">
+                    <a href="https://github.com/Hacking-NASSA-with-HTML/sailor-welder/" target="_blank" rel="noopener noreferrer">Give it a Star on github</a>
                 </p>
                 <p>Know how to improve it?</p>
                 <p>Send a pull request to the GitHub!</p>
