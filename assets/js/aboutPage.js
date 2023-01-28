b8c14bb78eb5b058bdd1d4f028efa467db2d342a
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-28 22:14:08 +0400

    feat: add my photo

--- assets/js/aboutPage.js
+++ assets/js/aboutPage.js
@@ -16,6 +16,9 @@ function aboutContent() {
                 <div class="about_img_wrapper">
                     <img data-src="./assets/img/anime_welder2_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder2_106_132.webp 1x, ./assets/img/anime_welder2_212_264.webp 2x" class="about_img lazyload" alt="One more anime sailor-welder">
                 </div>
+                <div class="about_img_wrapper">
+                    <img data-src="./assets/img/me_in_the_cabin_424_565.webp" width="106" height="132" srcset="./assets/img/me_in_the_cabin_424_565.webp 1x, ./assets/img/me_in_the_cabin_raw_848_1130.webp 2x" class="about_img lazyload" alt="Me in the cabin">
+                </div>
                 <p>About Page Content</p>
                 <p>About Page Content</p>
                 <p>About Page Content</p>
