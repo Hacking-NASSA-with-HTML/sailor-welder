d2e41e972ba48a2da03aa375a551d2e8aa590800
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-29 19:07:22 +0400

    refactor: change paragraphs styling

--- assets/js/aboutPage.js
+++ assets/js/aboutPage.js
@@ -5,27 +5,27 @@ function aboutContent() {
     <section id="about">
         <div class="about__wrapper">
             <div class="about_text_block">
-                <p>About Page Content</p>
-                <p>About Page Content</p>
-                <p>About Page Content</p>
-                <p>About Page Content</p>
-                <p>About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
                 <div class="about_img_wrapper">
                     <img data-src="./assets/img/anime_welder_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder_106_132.webp 1x, ./assets/img/anime_welder_212_264.webp 2x" class="about_img lazyload" alt="Anime sailor-welder">
                 </div>
-                <p>About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
                 <div class="about_img_wrapper">
                     <img data-src="./assets/img/anime_welder2_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder2_106_132.webp 1x, ./assets/img/anime_welder2_212_264.webp 2x" class="about_img lazyload" alt="One more anime sailor-welder">
                 </div>
-                <p>About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
                 <div class="about_img_wrapper">
                     <img data-src="./assets/img/me_in_the_cabin_424_565.webp" width="106" height="132" srcset="./assets/img/me_in_the_cabin_424_565.webp 1x, ./assets/img/me_in_the_cabin_raw_848_1130.webp 2x" class="about_img lazyload" alt="Me in the cabin">
                 </div>
-                <p>Me in the cabin</p>
-                <p>About Page Content</p>
-                <p>About Page Content</p>
-                <p>About Page Content</p>
-                <p>About Page Content</p>
+                <p class="darkblue_module_paragraph">Me in the cabin</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">About Page Content</p>
             </div>
         </div>
     </section>
--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -6,14 +6,14 @@ function homeContent() {
         <div class="home__wrapper">
             <div class="home_text_block">
                 <p class="welcome_home_module_paragraph">Welcome 👋</p>
-                <p class="home_module_paragraph">You are on the page of</p>
+                <p class="darkblue_module_paragraph">You are on the page of</p>
                 <h1 class="headline_h1">
                     Vyacheslav Plekhanov ⛵ Sailor-Welder 🎇 Resume 😎
                 </h1>
-                <p class="home_module_paragraph">and you can learn here</p>
-                <p class="home_module_paragraph">how we can earn money</p>
-                <p class="home_module_paragraph">with good ship's welding maintenance</p>
-                <p class="home_module_paragraph">or Your Dream's App web development🙂</p>
+                <p class="darkblue_module_paragraph">and you can learn here</p>
+                <p class="darkblue_module_paragraph">how we can earn money</p>
+                <p class="darkblue_module_paragraph">with good ship's welding maintenance</p>
+                <p class="darkblue_module_paragraph">or Your Dream's App web development🙂</p>
             </div>
         </div>
     </section>
