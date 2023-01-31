5efe1b0474b6b74c1ea21d0a81caf5cd7b1bad8a
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-31 15:01:28 +0400

    refactor: update resume text About section

--- assets/js/aboutPage.js
+++ assets/js/aboutPage.js
@@ -5,6 +5,7 @@ function aboutContent() {
     <section id="about">
         <div class="about__wrapper">
             <div class="section_text_block">
+                <p class="darkblue_module_paragraph">Problem-Solve Minded a Reliable ⛵ Sailor-Welder🎇 Navy Fitter </p>
                 <p class="darkblue_module_paragraph">I have worked in the welding </p>
                 <p class="darkblue_module_paragraph">and maritime industry </p>
                 <p class="darkblue_module_paragraph">for several years.</p>
@@ -15,25 +16,20 @@ function aboutContent() {
                 <p class="darkblue_module_paragraph">using computers and hand tools.</p>
                 <p class="darkblue_module_paragraph">A hard worker who is </p>
                 <p class="darkblue_module_paragraph">reliable and detail oriented.</p>
-                <p class="darkblue_module_paragraph">Interested in any opportunities </p>
+                <p class="darkblue_module_paragraph">Interested in opportunities </p>
                 <p class="darkblue_module_paragraph">that are available in </p>
                 <p class="darkblue_module_paragraph">the maritime industry.</p>
                 <div class="about_img_wrapper">
-                    <img data-src="./assets/img/anime_welder_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder_106_132.webp 1x, ./assets/img/anime_welder_212_264.webp 2x" class="about_img lazyload" alt="Anime sailor-welder">
+                    <img data-src="./assets/img/anime_welder2_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder2_106_132.webp 1x, ./assets/img/anime_welder2_212_264.webp 2x" class="about_img lazyload" alt="Anime Navy Fitter Sailor Welder in Merchant Navy at Work">
                 </div>
-                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">Navy Fitter Sailor ⛵ Welder in Merchant Navy at Work</p>
                 <div class="about_img_wrapper">
-                    <img data-src="./assets/img/anime_welder2_106_132.webp" width="106" height="132" srcset="./assets/img/anime_welder2_106_132.webp 1x, ./assets/img/anime_welder2_212_264.webp 2x" class="about_img lazyload" alt="One more anime sailor-welder">
+                    <img data-src="./assets/img/me_in_the_cabin_424_565.webp" width="106" height="132" srcset="./assets/img/me_in_the_cabin_424_565.webp 1x, ./assets/img/me_in_the_cabin_raw_848_1130.webp 2x" class="about_img lazyload" alt="Navy Fitter Sailor Welder in Merchant Navy in the cabin">
                 </div>
-                <p class="darkblue_module_paragraph">About Page Content</p>
-                <div class="about_img_wrapper">
-                    <img data-src="./assets/img/me_in_the_cabin_424_565.webp" width="106" height="132" srcset="./assets/img/me_in_the_cabin_424_565.webp 1x, ./assets/img/me_in_the_cabin_raw_848_1130.webp 2x" class="about_img lazyload" alt="Me in the cabin">
-                </div>
-                <p class="darkblue_module_paragraph">Me in the cabin</p>
-                <p class="darkblue_module_paragraph">About Page Content</p>
-                <p class="darkblue_module_paragraph">About Page Content</p>
-                <p class="darkblue_module_paragraph">About Page Content</p>
-                <p class="darkblue_module_paragraph">About Page Content</p>
+                <p class="darkblue_module_paragraph">Vyacheslav Plekhanov Sailor ⛵ Welder 🎇 Navy Fitter </p>
+                <p class="darkblue_module_paragraph">in Merchant Navy in the cabin.</p>
+                <p class="darkblue_module_paragraph">Need Getting Your Tasks Done?</p>
+                <p class="darkblue_module_paragraph">Let's Talk 📞 📩 !</p>
             </div>
         </div>
     </section>
