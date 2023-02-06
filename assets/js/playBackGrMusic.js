dd6b638b268a8ee81aff9eb07eae7569a48da243
J.Garcia <33934969+JuniorGarciaRodriguez@users.noreply.github.com>
2023-02-06 01:16:38 -0600

    Fixed Audio Switcher

--- assets/js/playBackGrMusic.js
+++ assets/js/playBackGrMusic.js
@@ -1,11 +1,9 @@
 // const PLAY = document.getElementById('play')
 // PLAY declared in headerScript.js file
-PLAY.addEventListener('click', () => handleOpenModal())
-
+PLAY.addEventListener("click", () => handleOpenModal());
 
 function renderModal() {
-    const renderedDivModal2 =
-        `
+    const renderedDivModal2 = `
         <div class='modal2'>
             <div class='modal-content'>
                 <div class="modalHeader">
@@ -18,85 +16,89 @@ function renderModal() {
                 </div>
             </div>
         </div>
-    `
-    return renderedDivModal2
+    `;
+    return renderedDivModal2;
 }
-root.insertAdjacentHTML("beforeend", renderModal())
+root.insertAdjacentHTML("beforeend", renderModal());
 
-
-const addToggleModalWindow = document.querySelector('.modal2')
-
+const addToggleModalWindow = document.querySelector(".modal2");
 
 function handleOpenModal() {
-    addToggleModalWindow.classList.add('openModalWin')
+    addToggleModalWindow.classList.add("openModalWin");
 
-    const closeModalButton = document.querySelector('.close-modal2')
-    closeModalButton.addEventListener('click', () => {
-        addToggleModalWindow.classList.remove('openModalWin')
-        addToggleModalWindow.classList.add('hideModalWin')
+    const closeModalButton = document.querySelector(".close-modal2");
+    closeModalButton.addEventListener("click", () => {
+        addToggleModalWindow.classList.remove("openModalWin");
+        addToggleModalWindow.classList.add("hideModalWin");
         setTimeout(() => {
-            addToggleModalWindow.classList.remove('hideModalWin')
-        }, 500)
-    })
+            addToggleModalWindow.classList.remove("hideModalWin");
+        }, 500);
+    });
 }
 
+let JoyfulMusic = document.querySelector(".Joyful");
+let MysticalMusic = document.querySelector(".Mystical");
 
-let JoyfulMusic = document.querySelector('.Joyful')
-let MysticalMusic = document.querySelector('.Mystical')
+let JoyfulMusicSound = new Audio("./assets/media/What-Do-We-Do.mp3");
+let MysticalMusicSound = new Audio("./assets/media/My-Jolly-Sailor-Bold.mp3");
 
-let JoyfulMusicSound = new Audio('./assets/media/What-Do-We-Do.mp3')
-let MysticalMusicSound = new Audio('./assets/media/My-Jolly-Sailor-Bold.mp3')
+let joyfulBackgroundMusic = true;
+let mysticalBackgroundMusic = true;
 
-let joyfulBackgroundMusic = true
-let mysticalBackgroundMusic = true
-
-
-JoyfulMusic.addEventListener('click', () => {
+JoyfulMusic.addEventListener("click", () => {
     if (joyfulBackgroundMusic) {
-        JoyfulMusicSound.volume = 0.25
-        JoyfulMusicSound.play()
-        joyfulBackgroundMusic = false
-        addToggleModalWindow.classList.remove('openModalWin')
-        addToggleModalWindow.classList.add('hideModalWin')
+        if (!mysticalBackgroundMusic) {
+            MysticalMusicSound.pause();
+            mysticalBackgroundMusic = true;
+        }
+        JoyfulMusicSound.volume = 0.25;
+        JoyfulMusicSound.play();
+        joyfulBackgroundMusic = false;
+        addToggleModalWindow.classList.remove("openModalWin");
+        addToggleModalWindow.classList.add("hideModalWin");
         setTimeout(() => {
-            addToggleModalWindow.classList.remove('hideModalWin')
-        }, 500)
+            addToggleModalWindow.classList.remove("hideModalWin");
+        }, 500);
     } else {
-        JoyfulMusicSound.pause()
-        joyfulBackgroundMusic = true
-        addToggleModalWindow.classList.remove('openModalWin')
-        addToggleModalWindow.classList.add('hideModalWin')
+        JoyfulMusicSound.pause();
+        joyfulBackgroundMusic = true;
+        addToggleModalWindow.classList.remove("openModalWin");
+        addToggleModalWindow.classList.add("hideModalWin");
         setTimeout(() => {
-            addToggleModalWindow.classList.remove('hideModalWin')
-        }, 500)
+            addToggleModalWindow.classList.remove("hideModalWin");
+        }, 500);
     }
-})
+});
 
-MysticalMusic.addEventListener('click', () => {
+MysticalMusic.addEventListener("click", () => {
     if (mysticalBackgroundMusic) {
-        MysticalMusicSound.volume = 0.25
-        MysticalMusicSound.play()
-        mysticalBackgroundMusic = false
-        addToggleModalWindow.classList.remove('openModalWin')
-        addToggleModalWindow.classList.add('hideModalWin')
+        if (!joyfulBackgroundMusic) {
+            JoyfulMusicSound.pause();
+            joyfulBackgroundMusic = true;
+        }
+        MysticalMusicSound.volume = 0.25;
+        MysticalMusicSound.play();
+        mysticalBackgroundMusic = false;
+        addToggleModalWindow.classList.remove("openModalWin");
+        addToggleModalWindow.classList.add("hideModalWin");
         setTimeout(() => {
-            addToggleModalWindow.classList.remove('hideModalWin')
-        }, 500)
+            addToggleModalWindow.classList.remove("hideModalWin");
+        }, 500);
     } else {
-        MysticalMusicSound.pause()
-        mysticalBackgroundMusic = true
-        addToggleModalWindow.classList.remove('openModalWin')
-        addToggleModalWindow.classList.add('hideModalWin')
+        MysticalMusicSound.pause();
+        mysticalBackgroundMusic = true;
+        addToggleModalWindow.classList.remove("openModalWin");
+        addToggleModalWindow.classList.add("hideModalWin");
         setTimeout(() => {
-            addToggleModalWindow.classList.remove('hideModalWin')
-        }, 500)
+            addToggleModalWindow.classList.remove("hideModalWin");
+        }, 500);
     }
-})
+});
 
-JoyfulMusicSound.addEventListener('ended', () => {
-    joyfulBackgroundMusic = true
-})
+JoyfulMusicSound.addEventListener("ended", () => {
+    joyfulBackgroundMusic = true;
+});
 
-MysticalMusicSound.addEventListener('ended', () => {
-    mysticalBackgroundMusic = true
-})
+MysticalMusicSound.addEventListener("ended", () => {
+    mysticalBackgroundMusic = true;
+});
