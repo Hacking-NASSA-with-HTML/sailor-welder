649534be7e534475cc72ae6c76e7ed6ccb8b3297
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-02-18 20:44:43 +0400

    refactor: change path syntax and cache key update

--- initServiceWorker.js
+++ initServiceWorker.js
@@ -1,16 +1,16 @@
-// const onWorkerReady = () => {
-//     console.log('Service Worker is ready')
-// }
+const onWorkerReady = () => {
+    console.log('Service Worker is ready')
+}
 
 // navigator.serviceWorker.register('/sailor-welder/assets/js/serviceWorker.js')
 // navigator.serviceWorker.register('/sailor-welder/serviceWorker.js')
 navigator.serviceWorker.register('/sailor-welder/serviceWorker.js')
-    .then(() => { console.log('Service Worker Finally Registered') })
+//     .then(() => { console.log('Service Worker Finally Registered') })
 
-// navigator.serviceWorker.ready.then(onWorkerReady)
+navigator.serviceWorker.ready.then(onWorkerReady)
 // navigator.serviceWorker.ready.then(onWorkerReady())
 // navigator.serviceWorker.ready.then(() => { console.log('Service Worker Finally Registered'); })
-// navigator.serviceWorker.then(() => { console.log('Service Worker Finally Registered'); })
+
 
 
 // from MDN tutorial
